5c1cf1dc9bd5d49185d6f480f5dc78f1e6d0dbb7
Swaraj Gosavi <106607805+swarajgosavi@users.noreply.github.com>
2023-08-09 13:14:28 +0530

    Updated Routes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,10 @@
-import React, { useState } from 'react';
+// Navbar.js
+import React from 'react';
 import { Link } from 'react-router-dom';
 import companyLogo from '../images/company-logo.png';
-import { FaUser, FaFileAlt, FaHome, FaSignOutAlt, FaBars, FaTimes } from 'react-icons/fa';
 import '../styles/Navbar.css';
-import '../styles/Sidebar.css';
 
 const Navbar = ({ loggedInUser }) => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [sidebarAnimation, setSidebarAnimation] = useState('');
-
-  const toggleSidebar = () => {
-    if (isSidebarOpen) {
-      setSidebarAnimation('slide-out');
-      setTimeout(() => {
-        setIsSidebarOpen(false);
-        setSidebarAnimation('');
-      }, 300); 
-    } else {
-      setIsSidebarOpen(true);
-      setSidebarAnimation('slide-in');
-    }
-  };
-
-
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
@@ -32,59 +14,40 @@ const Navbar = ({ loggedInUser }) => {
         <button
           className="navbar-toggler"
           type="button"
-          onClick={toggleSidebar}
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-          <FaBars className="navbar-toggler-icon" />
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div className={`sidebar ${isSidebarOpen ? 'open' : ''} ${sidebarAnimation}`}>
-          <div className="sidebar-header">
-
-            {loggedInUser && (
-              <div className="user-profile">
-                <img src={loggedInUser.profilePic} alt="Profile" className="profile-pic" />
-                <span className="user-name">{loggedInUser.name}</span>
-              </div>
-            )}
-            <button className="close-btn" onClick={toggleSidebar}>
-              <FaTimes />
-            </button>
-          </div>
-          <ul className="sidebar-menu">
-            <li>
-              <FaHome className="menu-icon" />
-              <span className="menu-text">Home</span>
-            </li>
-            <li>
-              <FaFileAlt className="menu-icon" />
-              <span className="menu-text">Job Description</span>
-            </li>
-            <li>
-              <FaFileAlt className="menu-icon" />
-              <span className="menu-text">Upload CVs</span>
-            </li>
-            <li>
-              <FaSignOutAlt className="menu-icon" />
-              <span className="menu-text">Logout</span>
-            </li>
-          </ul>
-        </div>
-        {/* Original content of the navbar */}
         <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link" to="/about">
-                About
+              <Link className="nav-link" to="/upload">
+                Upload
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/contact">
-                Contact
+              <Link className="nav-link" to="/candidates">
+              Candidates
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/test">
+              Test
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/instructions">
+              Instructions
               </Link>
             </li>
             {loggedInUser ? (
               <li className="nav-item">
                 <Link className="nav-link" to="/profile">
-                  <FaUser className="fas fa-user" /> {loggedInUser.name}
+                  <i className="fas fa-user"></i> {loggedInUser.name}
                 </Link>
               </li>
             ) : (
--- src/index.js
+++ src/index.js
@@ -11,9 +11,10 @@ import Candidates from './components/Candidates';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <Router>
+  <App />
+ </Router>
+
 );
 
 
